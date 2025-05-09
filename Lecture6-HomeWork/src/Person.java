b326e7bac7044cad1662bbed6c647b9138b84575
Vitalii Stankov <vitalii.stankov@gmail.com>
2025-05-09 14:42:01 +0300

    First Commit

---
+++ Lecture6-HomeWork/src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+
+    String name;
+    int age;
+    boolean isMan;
+
+    // Единствен конструктор, с параметри за всичките полета на класа
+    public Person(String name, int age, boolean isMan) {
+        this.name = name;
+        this.age = age;
+        this.isMan = isMan;
+    }
+    // Метод showPersonInfo, който показва информация за човека (изписва на конзолата стойността на всичките му полета по подходящ начин)
+    public void showPersonInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Is Man: " + isMan);
+    }
+
+    public double calculateOvertime(double hours) {
+        return 0.0; // Person няма overtime
+    }
+}
