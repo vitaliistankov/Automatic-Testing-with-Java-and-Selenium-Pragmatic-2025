e17b1dc30de46a97b7f9af4e809b18ea8e2663b3
Vitalii Stankov <vitalii.stankov@gmail.com>
2025-05-09 19:06:09 +0300

    Add Lecture3-HomeWork folder with source files

---
+++ Lecture3-HomeWork/src/Computer.java
@@ -0,0 +1,25 @@
+public class Computer {
+
+    int year;
+    double price;
+    boolean isNotebook;
+    double hardDiskMemory;
+    double freeMemory;
+    String operationSystem;
+
+
+    void changeOperationSystem(String newOperationSystem) {
+        operationSystem = newOperationSystem;
+    }
+
+    void useMemory(double memory) {
+        freeMemory -= memory;
+
+        if (memory > freeMemory) {
+            System.out.println("Not enough free memory!");
+        } else {
+            System.out.println("An app is running! Now only " + freeMemory + " free memory left.");
+        }
+    }
+
+}
