cfa2da31ca79b437670fb4d98b8a02429d9d5064
Vitalii Stankov <vitalii.stankov@gmail.com>
2025-06-08 22:11:07 +0300

    L15

---
+++ Lecture15-HomeWork/src/test/java/bg/pragmatic/MyFirstTest.java
@@ -0,0 +1,24 @@
+package bg.pragmatic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        this.driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("http://auto.pragmatic.bg/manage");
+
+    }
+}
+
