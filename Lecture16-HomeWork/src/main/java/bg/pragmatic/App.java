41a22d468546071cefd16e42d6765fa28f4e074d
Vitalii Stankov <vitalii.stankov@gmail.com>
2025-07-02 10:39:59 +0300

    Commit entire project

---
+++ Lecture16-HomeWork/src/main/java/bg/pragmatic/App.java
@@ -0,0 +1,13 @@
+package bg.pragmatic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
